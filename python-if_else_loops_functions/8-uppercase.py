cf419955578c3a1a8c2dd1d83e1467138ea2f911
Ace <acequantumbit@gmail.com>
2024-01-04 14:03:11 -0800

    line break

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        print("{}".format(chr(ord(letter) - 32) if 96 <= ord(letter) <= 123 else letter), end='')
+        print("{}".format(chr(ord(letter) - 32)\
+         if 96 <= ord(letter) <= 123 else letter), end='')
     print()
