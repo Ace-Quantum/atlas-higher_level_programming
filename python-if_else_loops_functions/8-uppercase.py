3b687b46fac7a1d723586914642fe9c87b0156ff
Ace <acequantumbit@gmail.com>
2024-01-04 13:56:51 -0800

    got rid of the end

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        print("{}".format(chr(ord(letter) - 32) if 96 <= ord(letter) <= 123 else letter), end='')
-    print("\n")
+        print("{}".format(chr(ord(letter) - 32) if 96 <= ord(letter) <= 123 else letter))
