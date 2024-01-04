decbdc80b961685d1a23da23a97511cd53d78aab
Ace <acequantumbit@gmail.com>
2024-01-04 13:51:53 -0800

    But whyyyyyyyy

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        if 96 <= ord(letter) <=123:
-            print("{}".format(chr(ord(letter) - 32)))
-        else:
-            print("{}".format(letter))
+        print("{}".format(chr(ord(letter) - 32) if 96 <= ord(letter) <= 123 else letter), end='')
+    print("\n")
