e97fa05a8cc117d03f54e535bafec4df6b0de033
Ace <acequantumbit@gmail.com>
2024-01-04 14:06:36 -0800

    i n d e n t

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(str):
     for letter in str:
         print("{}".format(chr(ord(letter) - 32)
-         if 96 <= ord(letter) <= 123 else letter), end='')
+        if 96 <= ord(letter) <= 123 else letter), end='')
     print()
