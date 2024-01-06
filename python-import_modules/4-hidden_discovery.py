773033aec18821a90dafe0b8ef36561d7e6e8e35
Ace <acequantumbit@gmail.com>
2024-01-05 18:45:40 -0800

    Okay but where the hell was I supposed to find that

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    print(dir(hidden_4))
+    for item in dir(hidden_4):
+        if item[0] != '_':
+            print("{}".format(item))
