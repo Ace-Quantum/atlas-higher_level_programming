c844a8aa302570c7732be262e43333f8dda1b318
Ace <acequantumbit@gmail.com>
2024-01-09 12:56:45 -0800

    we got this!

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    i = 0
     for row in matrix:
         for x in row:
-            print("{:d}".format(x), end=' ')
+            print("{:d}".format(x), end=' ' if i != 2 else '')
+            i = i + 1
+        i = 0
         print()
