4e30ffd081bd0442bc659a891b5c85a536b93de8
Ace <acequantumbit@gmail.com>
2024-01-09 13:01:12 -0800

    maybe this is the issue?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     i = 0
     for row in matrix:
         for x in row:
-            print("{:d}".format(x), end=' ' if i != 2 else '')
+            print("{}".format(x), end=' ' if i != 2 else '')
             i = i + 1
         i = 0
         print()
