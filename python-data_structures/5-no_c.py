533ab6a07cba9cbda28371a5a35911b33ab0253d
Ace <acequantumbit@gmail.com>
2024-01-09 12:33:12 -0800

    not taking out Cs

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for x in range(len(new_string)):
+    new_string = ''
+    for x in range(len(my_string)):
         if my_string[x] != "c" or my_string[x] != "C":
             new_string += my_string[x]
     return new_string
