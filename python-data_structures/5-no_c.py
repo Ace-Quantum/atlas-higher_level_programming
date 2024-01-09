df937faaba4997869a9f78a2aec7f94da3ff5dab
Ace <acequantumbit@gmail.com>
2024-01-09 12:31:11 -0800

    lol I haven't even tested this

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for x in range(len(new_string)):
         if my_string[x] != "c" or my_string[x] != "C":
-            new_string.append(
+            new_string += my_string[x]
     return new_string
