afd935f7e3ecbd92ec35c89f21425ab775866003
Ace <acequantumbit@gmail.com>
2024-01-09 06:41:40 -0800

    okay this should be it

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
+    def print_list_integer():
         for x in my_list:
-            print(x)
+            print("{}".format(x))
