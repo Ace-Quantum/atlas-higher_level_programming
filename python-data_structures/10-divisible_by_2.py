a83740de759dad2c1c49fd7672d015678e202888
Ace <acequantumbit@gmail.com>
2024-01-09 14:23:30 -0800

    I really think there's something up with the stester code

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
    new_list=[]
-   for x in range(0, len(my_list) - 1):
+   for x in my_list:
        if x % 2 == 0:
-           new_list[x] = True
+           new_list.append(True)
        else:
-            new_list[x] = False
+           new_list.append(False)
