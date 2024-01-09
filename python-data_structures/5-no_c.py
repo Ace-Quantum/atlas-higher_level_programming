41f649d11de706e457225452c12c2dfd711b1007
Ace <acequantumbit@gmail.com>
2024-01-09 12:29:54 -0800

    idk how to append

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = list(my_string)
-    for x in range(0, (len(new_string) - 1)):
-        if new_string[x - 1] == "c" or new_string[x - 1] == "C":
-            del new_string[x - 1]
+    for x in range(len(new_string)):
+        if my_string[x] != "c" or my_string[x] != "C":
+            new_string.append(
     return new_string
