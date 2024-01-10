041787c365778e519a6267b80961f312ce1ced8e
Ace <acequantumbit@gmail.com>
2024-01-09 18:12:35 -0800

    picking up speed

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    tester = 0
+    count = 0
+    for x in my_list:
+        try:
+            tester = my_list[x] + 0
+            print("{:d}".format(my_list[x]), end='')
+            count += 1
+        except:
+            break
+    print()
+    return count
