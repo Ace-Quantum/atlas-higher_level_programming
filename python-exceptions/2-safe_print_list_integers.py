c05c32ac55c0b3a0442e02828b88fa80f32b2e77
Ace <acequantumbit@gmail.com>
2024-01-09 18:15:52 -0800

    maybe it's a range issue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     tester = 0
     count = 0
-    for x in my_list:
+    for i in range(0, x):
         try:
             tester = my_list[x] + 0
             print("{:d}".format(my_list[x]), end='')
