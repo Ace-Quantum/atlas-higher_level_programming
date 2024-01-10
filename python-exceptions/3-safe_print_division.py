127a79c0efd0970537b5d4fc932367d57d43e21a
Ace <acequantumbit@gmail.com>
2024-01-09 18:30:05 -0800

    that should do it

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    c = 0
+    try:
+        c = a / b
+    except ValueError:
+        return None
+    finally:
+        print("Inside result: {:.2f}".format(c))
+        return c
