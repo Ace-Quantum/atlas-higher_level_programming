011d41e825926022bf143dcbe1bb0840fd444dcb
Ace <acequantumbit@gmail.com>
2024-01-09 18:17:03 -0800

    updated my vars

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
-            tester = my_list[x] + 0
-            print("{:d}".format(my_list[x]), end='')
+            tester = my_list[i] + 0
+            print("{:d}".format(my_list[i]), end='')
             count += 1
             tester = 0
         except:
