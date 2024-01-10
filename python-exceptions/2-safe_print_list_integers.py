e01b79259628e841efdc0557704ee337c599fabb
Ace <acequantumbit@gmail.com>
2024-01-09 18:19:13 -0800

    here we go, I've got a better understanding now

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
             tester = 0
-        except:
-            break
+        except (ValueError, TypeError):
+            continue
     print()
     return count
