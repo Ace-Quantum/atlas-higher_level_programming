c45265d13df652628c62add657c019ef2862b67f
Ace-Quantum <acequantumbit@gmail.com>
2024-01-15 23:39:28 -0800

    okay this is it

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,9 +4,8 @@ I'm not sure how to document this
 """
 class Square:
     """I imagine I need something here too"""
-    __size = None
+    _Square__size = None
 
     def __init__(self, size_input=None):
-        self.is_new = True
         if size_input is not None:
-            self.size = size_input
+            self._Square__size = size_input
