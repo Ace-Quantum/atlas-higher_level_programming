272315839266ccb1758b65b7d564c2e7d8d193cc
Ace-Quantum <acequantumbit@gmail.com>
2024-01-16 12:35:56 -0800

    I will find a way to make that initial idea for a loop work but not today

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+I'm not sure how to document this
+"""
+class Square:
+    """I imagine I need something here too"""
+
+    def __init__(self, size=0):
+        if size is not None:
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end='')
+                print()
