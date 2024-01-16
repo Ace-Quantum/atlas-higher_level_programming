7b15924d497a25b1a4b81e3c36a9085a94c84a35
Ace-Quantum <acequantumbit@gmail.com>
2024-01-16 11:56:58 -0800

    I don't remember a single thing about decorators

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,16 +4,29 @@ I'm not sure how to document this
 """
 class Square:
     """I imagine I need something here too"""
-    _Square__size = None
 
-    def __init__(self, size_input=0):
-        if size_input is not None:
-            if type(size_input) != int:
+    def __init__(self, size=0):
+        if size is not None:
+            if type(size) != int:
                 raise TypeError("size must be an integer")
-            elif size_input < 0:
+            elif size < 0:
                 raise ValueError("size must be >= 0")
             else:
-                self._Square__size = size_input
+                self.__size = size
 
     def area(self):
-        return (self._Square__size ** 2)
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
