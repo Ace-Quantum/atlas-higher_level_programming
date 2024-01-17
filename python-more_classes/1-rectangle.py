9c58b8d78c3a3ab648317908bc25fb74f3498afe
Ace-Quantum <acequantumbit@gmail.com>
2024-01-16 16:18:12 -0800

    forgot to double check error message

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,14 +11,14 @@ class Rectangle:
         if type(width) != int:
             raise TypeError("width must be an integer")
         elif width < 0:
-            raise ValueError("Width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = width
         
         if type(height) != int:
             raise TypeError("height must be an integer")
         elif height < 0:
-            raise ValueError("Width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = height
 
@@ -44,6 +44,6 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
