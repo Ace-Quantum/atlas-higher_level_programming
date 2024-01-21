f470438d90ebb32fc16f5959eb16dc30e6000ab5
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 14:43:52 -0800

    Documented file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This is a sub class of list made to print a sorted list"""
 class MyList(list):
+    """The sub class in question"""
     def print_sorted(self):
         print(sorted(self))
