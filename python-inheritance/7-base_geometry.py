14a42ee4a8b9ef5627a2ddc805baed4b66fa2d19
Ace-Quantum <acequantumbit@gmail.com>
2024-01-22 11:38:18 -0800

    wait so is that all they're asking for?

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is a module that holds a class
+"""
+
+
+class BaseGeometry:
+    """
+    I legit don't know what I'm doing here
+    """
+
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
