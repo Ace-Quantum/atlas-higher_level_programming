cf72a0b120eba4a08977ef3afd2e403ac7a730af
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 17:35:08 -0800

    Guess who always forgets documentation?

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""a module for a function"""
+
+
 def is_kind_of_class(obj, a_class):
+    """a function to return true if obj is an instance of a_class"""
     return isinstance(obj, a_class)
