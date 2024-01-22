2dcd332c9bda9275a75b8765eb8c3a996bf0e650
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 17:12:14 -0800

    Ugh style changes are getting me

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A module to hold a function"""
+
+
 def is_same_class(obj, a_class):
     """a function to return if an object is an instance"""
     return (type(obj) is a_class)
