d4206d08182cf148e359ebaefbf1ae8167873280
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 17:09:22 -0800

    added type() to the return

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 """A module to hold a function"""
 def is_same_class(obj, a_class):
     """a function to return if an object is an instance"""
-    return isinstance(obj, a_class)
+    return isinstance(type(obj), a_class)
