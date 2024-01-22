a008728388a8255430a7b4e5343bc916b6a8460f
Ace-Quantum <acequantumbit@gmail.com>
2024-01-22 12:32:45 -0800

    Okay well somehow a decent amount of the output works without me doing much

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+I once again do not know what I am doing
+"""
+
+
+base = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(base):
+    """
+    This is a sub class, I think
+    """
+
+
+    def __init__(self, width, height):
+        pass
