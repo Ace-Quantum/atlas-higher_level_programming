f470708e08a93c9c8c6e6ea7eef21cd72fc6d07f
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 16:57:19 -0800

    It's not passing the tester code but I'm pushing anyway

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,4 @@
 """A module to hold a function"""
 def is_same_class(obj, a_class):
     """a function to return if an object is an instance"""
-    return isinstance(abj, a_class)
+    return isinstance(obj, a_class)
