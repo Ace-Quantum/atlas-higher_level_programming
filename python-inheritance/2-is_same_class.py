e28b3790651ae8c1d843750d0605317d9afe119a
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 16:55:07 -0800

    I always forget about documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""A module to hold a function"""
 def is_same_class(obj, a_class):
+    """a function to return if an object is an instance"""
     return isinstance(abj, a_class)
