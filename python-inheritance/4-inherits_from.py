465ad40b986ad4ffad329558f1f9f7a7f94ccade
Ace-Quantum <acequantumbit@gmail.com>
2024-01-21 17:57:59 -0800

    Okay yeah no I don't get this one

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,9 @@
 
 def inherits_from(obj, a_class):
     """idk, check if the class from the object is a sub class of the specified class
-    Ah, okay so type will return the class. That's how it's worked this whole time. Got it."""
-    return issubclass(type(obj), a_class)
+    Ah, okay so type will return the class. That's how it's worked this whole time. Got it.
+    Actually I don't get it??? Why does this need to be in an if statement?"""
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
