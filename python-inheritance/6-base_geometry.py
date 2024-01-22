42b74b913f8bbdee90761ded12b63e5c7a33cbb7
Ace-Quantum <acequantumbit@gmail.com>
2024-01-22 11:02:58 -0800

    I'm so sick of this that I'm going to make new resources for students to use.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a module that holds a class
+"""
+
+
+class BaseGeometry:
+    """
+    I legit don't know what I'm doing here
+    """
+
+
+    def area(self):
+        raise Exception("area() is not implemented")
