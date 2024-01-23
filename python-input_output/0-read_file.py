ea094470408a6765a43e861e74e18db7b7f7f3dc
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 15:05:14 -0800

    added documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This is some documentation
+"""
+
+
 def read_file(filename=""):
+    """
+    This is more documentation
+    """
+
+
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
