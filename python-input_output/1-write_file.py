85dc7abe52abb0dc435cf978fca6b61f04e44274
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 15:24:42 -0800

    Feeling good about this one

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is some documentation
+"""
+
+def write_file(filename="", text=""):
+    """
+    This is more documentation
+    """
+
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+    return len(text)
