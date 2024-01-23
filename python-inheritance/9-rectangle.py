f759260be451b52cbbda32701f0ec1d192252ca2
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 13:30:07 -0800

    Okay that's easier than expected

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+I once again do not know what I am doing
+"""
+
+
+base = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(base):
+    """
+    This is a sub class, I think
+    """
+
+
+    def __init__(self, width, height):
+        __width = base.integer_validator(self, "width", width)
+        __height = base.integer_validator(self, "height", height)
+
+    def __str__(self):
+
