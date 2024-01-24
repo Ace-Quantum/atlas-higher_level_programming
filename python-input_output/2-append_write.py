7de84412c7434128590675c9178363148983ef93
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 16:02:46 -0800

    okay neat, this isn't that bad!

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this is documentation
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    This is more documentation
+    """
+
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text)
+
+    return len(text)
