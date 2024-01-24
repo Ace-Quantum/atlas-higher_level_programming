1cb3e3b874f4b4137db279df79b6104b2a5b510c
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 18:30:05 -0800

    help

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 """
 do some documentation
+here's some more I guess
+Idk what else to do with this
 """
-
 
 import sys
 from os.path import isfile
-
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
