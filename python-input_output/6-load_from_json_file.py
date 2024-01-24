c59eae9ec8531ff5ad536ab286c9a38acb5fae5c
Ace-Quantum <acequantumbit@gmail.com>
2024-01-23 17:13:40 -0800

    parenthesis

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Here's more documentation
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    And some more
+    """
+
+    with open(filename, 'r') as f:
+        return json.loads(f.read())
