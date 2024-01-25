d2ade26fe209a1488db1114b602bacbe707e8078
Ace <AceQuantumBit@gmail.com>
2024-01-25 13:34:45 -0600

    Just try it, right?

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Making a sub sub class
+"""
+
+rec = __import__('9-rectangle').Rectangle
+
+class Square(rec):
+    """
+    This is a sub class of rectangle
+    It shows a square
+    """
+
+
+    def __init__(self, size):
+        self.__size = base.integer_validator(self, "size", size)
+
+    def area(self, size):
+        return self.__size ** 2
