4fa32c22f2f696f6037ad4089f01484627d95f68
Ace <AceQuantumBit@gmail.com>
2024-01-25 11:10:03 -0600

    Finally remembered how to format files

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This is a module for returning if obj belongs to a subclass of specified class"""
 
+
 def inherits_from(obj, a_class):
     """idk, check if the class from the object is a sub class of the specified class
     Ah, okay so type will return the class. That's how it's worked this whole time. Got it.
     Actually I don't get it??? Why does this need to be in an if statement?"""
-    if issubclass(type(obj), a_class):
-        return True
-    else:
+
+    if type(obj) is a_class:
         return False
+    return isinstance(obj, obj, a_class)
