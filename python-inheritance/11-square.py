2aed0fa261f02b6bd882f02b123199f8459a98f9
Ace <AceQuantumBit@gmail.com>
2024-01-25 14:12:20 -0600

    Added str method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Making a sub sub class
+"""
+
+rec = __import__('9-rectangle').Rectangle
+
+class Square(rec):
+    """
+    This is a sub class of rectangle
+    It shows a square
+    """
+
+
+    def __init__(self, size):
+        self.__size = self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size ** 2
+    
+    def __str__(self):
+        return "[Square] {0}/{1}".format(self.__size, self.__size)
