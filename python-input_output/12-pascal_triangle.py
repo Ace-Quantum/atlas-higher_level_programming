400e60d0c0bfe750a9bb5933b86e820e030df1b9
Ace <AceQuantumBit@gmail.com>
2024-01-26 14:46:56 -0600

    Don't tell me the order in the if statement is an issue

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,7 +18,7 @@ def pascal_triangle(n):
     for i in range(n):
         current_layer = []
         for j in range(i+1):
-            if j < i and i > 0 and j > 0:
+            if j > 0 and j < i and i > 0:
                 current_layer.append(ret_list[i-1][j-1] + ret_list[i-1][j])
             else:
                 current_layer.append(1)
