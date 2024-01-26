3e16dd100bfcdb514889e55fddc3a97b082a4f70
Ace <AceQuantumBit@gmail.com>
2024-01-26 13:03:20 -0600

    Indented

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,9 +6,9 @@ Now we're getting into classes and JSON
 class Student:
 
 
-"""
-here's some documentation!
-"""
+    """
+    here's some documentation!
+    """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
