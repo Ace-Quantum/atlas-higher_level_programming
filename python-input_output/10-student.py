c8ede374b621f05c9ca6ff0e9d0dfc790e9c6fad
Ace <AceQuantumBit@gmail.com>
2024-01-26 13:02:26 -0600

    Here's some documentation!

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,13 +6,17 @@ Now we're getting into classes and JSON
 class Student:
 
 
+"""
+here's some documentation!
+"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if type(attrs) is list and all(type(elm) = str for elm in attrs)
+        if type(attrs) is list and all(type(elm) = str for elm in attrs):
             return True
         else:
             return vars(self)
