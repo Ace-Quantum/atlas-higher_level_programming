a9b37fd72cc6fcff7a7809432ce06495295b772c
Ace <AceQuantumBit@gmail.com>
2024-01-26 15:00:41 -0600

    formatting

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,13 +3,11 @@
 Now we're getting into classes and JSON
 """
 
-
 class Student:
 
     """
     here's some documentation!
     """
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -19,7 +17,7 @@ class Student:
         current_attrs = vars(self)
         ret_dict = dict()
 
-        if type(attrs) is list and all(type(elm) == str for elm in attrs):
+        if type(attrs) is list and all(type(elm) is str for elm in attrs):
             for elm in attrs:
                 if hasattr(self, elm):
                     ret_dict[elm] = current_attrs[elm]
