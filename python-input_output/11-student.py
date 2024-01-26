1f94a90102fc72c29be2d042dfcaa222bab31825
Ace <AceQuantumBit@gmail.com>
2024-01-26 13:48:48 -0600

    friggin idk what the heck is happening

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Now we're getting into classes and JSON
+"""
+
+class Student:
+
+
+    """
+    here's some documentation!
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        current_attrs = vars(self)
+        ret_dict = dict()
+
+        if type(attrs) is list and all(type(elm) == str for elm in attrs):
+            for elm in attrs:
+                if hasattr(self, elm):
+                    ret_dict[elm] = current_attrs[elm]
+            return ret_dict
+        else:
+            return vars(self)
+        
+    def reload_from_json(self, json):
+        new_attrs = json.dumps(json)
+
+        for elm in new_attrs:
+            self[elm] = new_attrs[elm]
