4185e020e8ea5428c51cbe6a817921e21f2e062d
Ace <AceQuantumBit@gmail.com>
2024-01-27 15:50:26 -0600

    capitalized None

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,7 +13,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != none:
+        if id != None:
             self.id = id
         else:
             self.__nb_objects += 1
