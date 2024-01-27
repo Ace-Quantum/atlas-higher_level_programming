20dd0b5cffcafd1470fc3ad8fcf1859ccb477143
Ace <AceQuantumBit@gmail.com>
2024-01-27 16:49:52 -0600

    It's mistakes like this that make us learn

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -45,7 +45,7 @@ class Rectangle(Base):
         self.__y = value
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.id = super().__init__(id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
