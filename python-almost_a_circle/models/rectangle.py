011e362e2f9140b8f0b182a8869649e5d8474939
Ace <AceQuantumBit@gmail.com>
2024-01-27 19:29:32 -0600

    Reverting changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -46,7 +46,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = Base.integer_validator("width", width)
-        self.__height = Base.integer_validator("height", height)
-        self.__x = Base.integer_validator("x", x)
-        self.__y = Base.integer_validator("y", y)
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+        self.__x = self.integer_validator("x", x)
+        self.__y = self.integer_validator("y", y)
