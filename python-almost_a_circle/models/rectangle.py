e4418ac997e9d492a8da7b213ee93dcebb63273a
Ace <AceQuantumBit@gmail.com>
2024-01-28 18:56:47 -0600

    Time to recruit help from a friend, but first let's see if this even works

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,9 +108,15 @@ class Rectangle(Base):
         """
         This method will update the value of the attributes
         """
-        
-        self.id = args[0]
-        self.__width = args[1]
-        self.__height = args[2]
-        self.__x = args[3]
-        self.__y = args[4]
+
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+            
