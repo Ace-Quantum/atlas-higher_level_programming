8568cf43ffc7706b96cdf69a7f949fdc62f98d7a
Ace <AceQuantumBit@gmail.com>
2024-01-30 08:32:07 -0600

    Ok so apparently it only wants width for some reason and I have no idea why

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,7 +21,7 @@ class Square(Rectangle):
         self.__size = self.integer_validator("size", value)
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, width=0, height=0, x=0, y=0)
+        super().__init__(id, width=0)
         self.__width = super.integer_validator("size", size)
         self.__height = super.integer_validator("size", size)
         self.__x = super.integer_validator("x", x)
