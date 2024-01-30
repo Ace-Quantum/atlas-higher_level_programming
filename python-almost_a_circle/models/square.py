ae7fc03b66c96b635e18b251f8f1896c1e4c042e
Ace <AceQuantumBit@gmail.com>
2024-01-30 13:51:16 -0600

    overrided str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,3 +23,12 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """
+        overiding str
+        """
+
+        return "[Square] ({0}) {1}/{2} - {3}".format(
+            self.id, self.x, self.y, self.size)
+        
+
