f7ec92de43c6a862263fbc8d625fbf26971f3b0b
Ace <AceQuantumBit@gmail.com>
2024-01-30 17:06:20 -0600

    save to file code created

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,3 +30,16 @@ class Base:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
+        
+    def save_to_file(cls, list_objs):
+        """
+        this will save into a json file
+        """
+
+        ret_list = []
+        if list_objs is not None:
+            for i in list_objs:
+                ret_list.append(i.to_dictionary())
+        with open(cls.__name__ + ".json", "w") as f:
+            f.write(cls.to_json_string(ret_list))
+            
