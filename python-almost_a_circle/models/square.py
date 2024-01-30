32099d35d7c5fd236856ab86fdf78f0e5b5e79db
Ace <AceQuantumBit@gmail.com>
2024-01-30 09:53:56 -0600

    Everytime I think I understand I get knocked down

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,5 +21,5 @@ class Square(Rectangle):
         self.__size = self.integer_validator("size", value)
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size)
+        super().__init__(size, size, x, y, id)
 
