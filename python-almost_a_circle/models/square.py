9ca4b31f1d84c36756b487d9b10cbaf3b5e2d596
Ace <AceQuantumBit@gmail.com>
2024-01-30 08:27:39 -0600

    No idea if it's right but pushing anyway

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,13 +4,25 @@
 time for squares
 """
 
-from models.base import Rectangle
+rec = __import__("rectangle").Rectangle
 
-class Square(Rectangle):
+class Square(rec):
 
     """
     What do I do
     """
 
+    @property
+    def size(self, value):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = self.integer_validator("size", value)
+
     def __init__(self, size, x=0, y=0, id=None):
-        
+        super().__init__(id)
+        self.__width = super.integer_validator("size", size)
+        self.__height = super.integer_validator("size", size)
+        self.__x = super.integer_validator("x", x)
+        self.__y = super.integer("y", y)
