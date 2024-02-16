0a15120f8e89a993fab6eda58ef9a91b248aed8a
root <root@DESKTOP-FADRK94>
2024-02-16 10:56:16 -0600

    gorsh dang it

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- grants all permissions to a specified user
 
 CREATE USER IF NOT EXISTS 'user_01_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
