81983840d79b92c5b9a20b82f9e97e3be06148d1
root <root@DESKTOP-FADRK94>
2024-02-16 12:00:03 -0600

    this is again definitely not it

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists cities by states I'm assuming
 
-SELECT cities.id, DISTINCT cities.name, states.name
+SELECT DISTINCT cities.id, cities.name, states.name
 FROM cities, states
 ORDER BY cities.id ASC
 ;
