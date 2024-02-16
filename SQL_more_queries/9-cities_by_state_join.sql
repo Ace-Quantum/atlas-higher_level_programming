1aef592ef3cf52b478543cd89e8ffc191d2882fa
root <root@DESKTOP-FADRK94>
2024-02-16 11:57:57 -0600

    added distinct on line 3

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists cities by states I'm assuming
 
-SELECT cities.id, cities.name, states.name
+SELECT cities.id, DISTINCT cities.name, states.name
 FROM cities, states
 ORDER BY cities.id ASC
-FETCH FIRST 1 ROWS ONLY;
+;
