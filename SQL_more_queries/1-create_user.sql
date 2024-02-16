5910246c7ad105efa1edb3827e9f100cffe36526
root <root@DESKTOP-FADRK94>
2024-02-16 09:12:07 -0600

    This is where the road gets bumpy

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- grants all permissions to a specified user
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
