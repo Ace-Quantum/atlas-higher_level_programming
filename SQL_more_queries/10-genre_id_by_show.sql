21db29e51ee870e8017181cf15e6016312f9286f
root <root@DESKTOP-FADRK94>
2024-02-16 12:28:02 -0600

    joins were never my strongsuit

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- show genra by id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
 WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
