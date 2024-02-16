152ce87cf6e6efbeacde98e6dfaa65a008207e37
root <root@DESKTOP-FADRK94>
2024-02-16 10:39:00 -0600

    please

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 -- grants all permissions to a specified user
 
-SELECT user FROM mysql.user;
-
 CREATE USER IF NOT EXISTS 'user_01_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
