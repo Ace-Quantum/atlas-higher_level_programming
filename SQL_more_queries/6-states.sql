f9e4a0d79d62bf1dcb6fac01b82c95238361472a
root <root@DESKTOP-FADRK94>
2024-02-16 11:34:24 -0600

    typo

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- makes a table auto gen blah blah blah
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_0_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_0_usa.states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
