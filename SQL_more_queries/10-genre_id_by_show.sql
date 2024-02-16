63a830cde9af8010f47c2829b97fbab0821b5a90
root <root@DESKTOP-FADRK94>
2024-02-16 14:16:41 -0600

    pushing before restarting vs code

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
+
 WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,2 @@
+-- forgot to do this one
+
