5fda12b0afb543ae1c26d601ba75ee2671de4711
root <root@DESKTOP-FADRK94>
2024-02-16 12:03:36 -0600

    I'm going to fight god

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- lists cities by states I'm assuming
 
-SELECT DISTINCT cities.id, cities.name, states.name
+SELECT DISTINCT cities.id, min(cities.name) AS cities.name, states.name
 FROM cities, states
-ORDER BY cities.id ASC
-;
+ORDER BY cities.id ASC;
