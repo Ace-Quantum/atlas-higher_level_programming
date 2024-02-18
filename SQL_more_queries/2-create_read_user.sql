21fb2f93e897c35bfe595f3a03ec156a0aa93961
root <root@DESKTOP-FADRK94>
2024-02-18 17:16:36 -0600

    lol another typo kms

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- forgot to do this one 
 
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT on htbn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
