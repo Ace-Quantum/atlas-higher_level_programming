32c93f77bd327684fb17e201560d55f549aa3111
root <root@DESKTOP-FADRK94>
2024-02-18 17:10:50 -0600

    I'm kinda fed up with this atm

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,3 +4,5 @@ CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT on htbn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
