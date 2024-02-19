fed9303919ccc6d31ad3049f5e3cebb9abe7ec20
root <root@DESKTOP-FADRK94>
2024-02-19 13:36:46 -0600

    switched order in query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("""
-    SELECT cities.id, states.name, cities.name 
+    SELECT cities.id, cities.name, states.name 
     FROM cities
     INNER JOIN states
     ON cities.state_id = states.id
