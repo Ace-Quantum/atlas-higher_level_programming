439531f2eb4a0d56ccd037b49af102dc517d04c7
root <root@DESKTOP-FADRK94>
2024-02-19 12:26:33 -0600

    Okay the output is good

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,6 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT name FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    
+    results = cur.fetchall()
 
-    print(cur._rows)
+    for state in results:
+        print(state)
