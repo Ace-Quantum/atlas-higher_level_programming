21fc874f84c870af8a1671b94751a21f094d3274
root <root@DESKTOP-FADRK94>
2024-02-19 12:44:41 -0600

    Okay maybe there is some hope

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Here is a script to list states by IDs
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("""
+    SELECT id, name 
+    FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY states.id ASC;""")
+    
+    results = cur.fetchall()
+
+    for state in results:
+        print(state)
