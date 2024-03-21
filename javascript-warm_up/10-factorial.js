d5759c7eaaeb460187987b8f942ce6831f339b2f
root <root@DESKTOP-FADRK94>
2024-03-21 14:41:03 -0500

    turned it into a function

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,13 +3,15 @@
 const args = process.argv;
 
 const num = (parseInt(args[2]) + 1);
-// console.log(num);
-let i = 1;
-let fact = 1;
 
-while (i < num) {
-  fact = fact * i;
-  i++;
+function factorial(num) {
+  let i = 1;
+  let fact = 1;
+  while (i < num) {
+    fact = fact * i;
+    i++;
+  }
+  console.log(fact);
 }
 
-console.log(fact);
+factorial(num)
