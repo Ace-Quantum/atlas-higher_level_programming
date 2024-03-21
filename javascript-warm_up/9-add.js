2b9a834ca0ff2d2946f9f6fc72940dccf7f679cf
root <root@DESKTOP-FADRK94>
2024-03-21 14:29:08 -0500

    styling

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 
-let a = parseInt(args[2]);
-let b = parseInt(args[3]);
+const a = parseInt(args[2]);
+const b = parseInt(args[3]);
 
 function add (a, b) {
   console.log(a + b);
