57487a584fd1e17ead5d7571338bb76e63c46349
root <root@DESKTOP-FADRK94>
2024-03-21 17:15:29 -0500

    Thank you Cody

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,10 +2,10 @@
 
 const args = process.argv;
 
-let isanumber = parseInt(args[2]);
+let isanumber = args[2];
 
-if (typeof isanumber === 'number') {
-  console.log('My number: ' + args[2]);
-} else {
+if (isNaN(isanumber)) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + args[2]);
 }
