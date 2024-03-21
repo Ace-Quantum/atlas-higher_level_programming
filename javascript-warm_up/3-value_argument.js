097edd76579472982cb6cdf8c4352fec514da3ec
root <root@DESKTOP-FADRK94>
2024-03-21 11:18:52 -0500

    Can't use var?

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const args = process.argv;
 if (args.length === 2) {
   console.log('No argument');
 } else {
-  for (var i = 0; i < args.length; i++) {
+  for (let i = 0; i < args.length; i++) {
     console.log(args[i]);
   }
 }
