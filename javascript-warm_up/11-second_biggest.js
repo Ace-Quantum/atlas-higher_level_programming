ad2120b35538ab41d38627cde370870149440693
root <root@DESKTOP-FADRK94>
2024-03-21 15:13:54 -0500

    camel case aparantly

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 let i = 0;
 let biggest = 0;
-let Second_Biggest = 0;
+let SecondBiggest = 0;
 
 if (args.length < 2) {
   console.log('0');
@@ -11,12 +11,12 @@ if (args.length < 2) {
 
 while (args[i] != null) {
   if (parseInt(args[i]) > biggest) {
-    Second_Biggest = biggest;
+    SecondBiggest = biggest;
     biggest = parseInt(args[i]);
-  } else if (parseInt(args[i]) > Second_Biggest) {
-    Second_Biggest = parseInt(args[i]);
+  } else if (parseInt(args[i]) > SecondBiggest) {
+    SecondBiggest = parseInt(args[i]);
   }
   i++;
 }
 
-console.log(Second_Biggest);
+console.log(SecondBiggest);
