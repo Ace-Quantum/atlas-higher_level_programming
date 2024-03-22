7373ae64ded505cb3a860cce4ea8dbe84ee9abdf
root <root@DESKTOP-FADRK94>
2024-03-22 14:26:53 -0500

    style fix

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 
-x = 0;
-
-// add();
+let x = 0;
 
 function add (a, b) {
-//   const args = process.argv;
-
-//   const a = parseInt(args[2]);
-//   const b = parseInt(args[3]);
   x = (a + b);
   return (x);
 }
