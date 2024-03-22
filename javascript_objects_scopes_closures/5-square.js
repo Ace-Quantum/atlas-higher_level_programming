c0968047a30e5deed7cf67c83d004d0b9797ad82
root <root@DESKTOP-FADRK94>
2024-03-22 15:50:39 -0500

    got it with some Ryan's help

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,53 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w < 1 || h < 1 || typeof (w) === 'undefined' || typeof (h) === 'undefined') {
+      // pass
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let str = '';
+    while (i < this.width) {
+      str = str + 'X';
+      i++;
+    }
+
+    i = 0;
+
+    while (i < this.height) {
+      console.log(str);
+      i++;
+    }
+  }
+
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+
+    this.height = w;
+    this.width = h;
+  }
+
+  double () {
+    const h = this.height;
+    const w = this.width;
+
+    this.height = (h * 2);
+    this.width = (w * 2);
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
