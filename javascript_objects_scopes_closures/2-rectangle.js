1f8be04b9f6d10935ed78dd970013dd3bcf5ca3c
root <root@DESKTOP-FADRK94>
2024-03-22 13:42:01 -0500

    plz no style errors

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
     constructor (w, h) {
-      if (w < 1 || h < 1) {
+      if (w < 1 || h < 1 || typeof(w) === 'undefined' || typeof(h) === 'undefined') {
       } else {
         this.width = w;
         this.height = h;
