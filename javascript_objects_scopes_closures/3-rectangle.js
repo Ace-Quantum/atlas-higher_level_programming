17601f3485d402716a25fd3d71b82103d52ee65e
root <root@DESKTOP-FADRK94>
2024-03-22 14:10:36 -0500

    finally

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w < 1 || h < 1 || typeof (w) === 'undefined' || typeof (h) === 'undefined') {
+        // pass
+      } else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print() {
+        let i = 0;
+        let str = ''
+        while (i < this.width) {
+          str = str + 'X'
+          i++;
+        }
+
+        i = 0;
+
+        while (i < this.height) {
+          console.log(str);
+          i++;
+        }
+      }
+  }
+  
+  module.exports = Rectangle;
+  
