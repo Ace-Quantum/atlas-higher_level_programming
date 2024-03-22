96ba14cc629804561083b7a342574cfcb0be6ea7
root <root@DESKTOP-FADRK94>
2024-03-22 14:40:11 -0500

    styling

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,48 +1,48 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w < 1 || h < 1 || typeof (w) === 'undefined' || typeof (h) === 'undefined') {
-        // pass
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w < 1 || h < 1 || typeof (w) === 'undefined' || typeof (h) === 'undefined') {
+      // pass
+    } else {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      let i = 0;
-      let str = '';
-      while (i < this.width) {
-        str = str + 'X';
-        i++;
-      }
-  
-      i = 0;
-  
-      while (i < this.height) {
-        console.log(str);
-        i++;
-      }
-    }
-
-    rotate () {
-      let h = this.height;
-      let w = this.width;
-
-      this.height = w;
-      this.width = h;
-    }
-
-    double () {
-      let h = this.height;
-      let w = this.width;
-
-      this.height = (h * 2);
-      this.width = (w * 2);
-    }
-
   }
+
+  print () {
+    let i = 0;
+    let str = '';
+    while (i < this.width) {
+      str = str + 'X';
+      i++;
+    }
+
+    i = 0;
+
+    while (i < this.height) {
+      console.log(str);
+      i++;
+    }
+  }
+
+  rotate () {
+    let h = this.height;
+    let w = this.width;
+
+    this.height = w;
+    this.width = h;
+  }
+
+  double () {
+    let h = this.height;
+    let w = this.width;
+
+    this.height = (h * 2);
+    this.width = (w * 2);
+  }
+
+}
   
-  module.exports = Rectangle;
+module.exports = Rectangle;
   
