45295303618edd4f90c8cef8b2431d64b91e5edc
root <root@DESKTOP-FADRK94>
2024-03-26 13:39:39 -0500

    made the thing

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+let counter = 0;
+
+exports.logMe = function (item) {
+    console.log(`${counter}: ${item}`);
+    counter++;
+}
+
+    //     let counter = 0;
+
+//     function printit () {
+//         console.log('${counter}: {item}');
+//         counter++;
+//     }
+
+//     return printit();
+// }
+
