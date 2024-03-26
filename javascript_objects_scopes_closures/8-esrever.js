56f93b9d4d4bed274005b84b5fc53b2bdbb79814
root <root@DESKTOP-FADRK94>
2024-03-26 14:10:16 -0500

    style

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-exports.esrever = function (list){
+exports.esrever = function (list) {
   let i = (list.length - 1);
-  let listreturn = [];
+  const listreturn = [];
 
-  while (i > - 1) {
+  while (i > -1) {
     listreturn.push(list[i]);
     i--;
   }
   return listreturn;
-}
+};
