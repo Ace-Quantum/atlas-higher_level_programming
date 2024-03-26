9c3a843c33f491373338fb81a8fb9366d0a04e47
root <root@DESKTOP-FADRK94>
2024-03-26 14:05:59 -0500

    f it I'm spamming the checker

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list){
-  let i = (list.length)
+  let i = (list.length - 1)
   let listreturn = []
 
-  while (i > 0) {
+  while (i > - 1) {
     listreturn.push(list[i]);
     i--;
   }
