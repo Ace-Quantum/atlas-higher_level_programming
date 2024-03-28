2761598e75eaacb2996d99b0968ea1cbe564e793
root <root@DESKTOP-FADRK94>
2024-03-28 10:35:33 -0500

    Well it's not working but still

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+// const fs = require('fs');
 const args = process.argv;
 const request = require('request');
 
-request(args[2], function(error, response) {
-    console.error('error', error);
+request(args[2], response => {
     console.log('code:' + response);
 })
