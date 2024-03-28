f032101eb801ef389c0b3ce9249bdb4e905f5bf2
root <root@DESKTOP-FADRK94>
2024-03-28 14:14:26 -0500

    style

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -8,8 +8,8 @@ const url = args[2];
 const FilePath = args[3];
 
 request(url, function (error, response, body) {
-    if (error) console.log(error);
-    fs.writeFile(FilePath, body, (err) => {
-        if (err) throw err;
-    })
-})
+  if (error) console.log(error);
+  fs.writeFile(FilePath, body, (err) => {
+    if (err) throw err;
+  });
+});
