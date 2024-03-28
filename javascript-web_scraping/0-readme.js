da8422c8cf350db11b40a44e9463045f9fe9d815
root <root@DESKTOP-FADRK94>
2024-03-28 10:04:56 -0500

    yes?

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,32 @@
 #!/usr/bin/node
 
-function ReadFile (InputFile) {
-  let file = window.open(InputFile);
+const fs = require('fs');
+const args = process.argv;
 
-  FileReader.readAsText(file);
-}
+// console.log('hello');
+
+fs.readFile(args[2], (err, content) => {
+    if (err) throw err;
+
+    console.log(content.toString());
+
+});
+
+
+// console.log('hello?');
+// const fs = require('fs');
+// let InputFile = new FileReader();
+
+// function TaskReadFile (InputFile) {
+//   fs.readFile(InputFile, (err, content) => {
+//     if (err) {
+//         console.error('error', err);
+//         return
+//     }
+//     console.log(content.toString());
+//     console.log('heck');
+//   })
+  
+// }
+
+// exports.TaskReadFile = TaskReadFile;
