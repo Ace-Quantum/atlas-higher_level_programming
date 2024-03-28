f7ad48831d2e718cdf74254f45aa864842b1b9a8
root <root@DESKTOP-FADRK94>
2024-03-28 12:04:03 -0500

    style

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,6 +6,7 @@ const args = process.argv;
 const StarWarsData = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 
 request(StarWarsData, function (error, response, body) {
+  if (error) console.log(error);
   const data = JSON.parse(body);
 
   console.log(data.title);
