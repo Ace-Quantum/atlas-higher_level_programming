b93c0b8fa8e60c34cc0ff3766fbecedda091879d
root <root@DESKTOP-FADRK94>
2024-03-28 15:03:41 -0500

    heck

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+const url = args[2];
+const listprint = {};
+let counter = 0;
+
+request(url, function (error, response, body) {
+    if (error) console.log(error);
+    const data = JSON.parse(body);
+    for (let key of data) {
+        let curID = key.userId;
+        while (key.userId === curID) {
+            if (key.completed === true) {
+                counter++;
+            }
+            key++;
+            console.log('key: ' + key.userId)
+        }
+        listprint[curID] = counter;
+        curID = key.userId;
+        counter = 0;
+    }
+    console.log(listprint)
+})
