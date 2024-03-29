d38ad89de7e9a521826feac3bef3e3564c6f5888
root <root@DESKTOP-FADRK94>
2024-03-29 12:06:48 -0500

    Thank you david

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,28 +4,30 @@ const request = require('request');
 const args = process.argv;
 
 const url = args[2];
+let counter = 0;
+let listprint = {};
 
 request(url, function (error, response, body) {
     if (error) console.log(error);
     const data = JSON.parse(body);
-    let counter = 0;
-    let listprint = {};
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length - 1; i++) {
         let curID = data[i].userId;
-        console.log("typof data[i]: " + typeof(data[i]))
-        console.log("curID : " + curID)
-        console.log("data " + i + " : " + data[i])
-        while (data[i].userId === curID || i < data.length) {
+        // console.log("typof data[i]: " + typeof(data[i]))
+        // console.log("curID : " + curID)
+        // console.log("data " + i + " : " + data[i])
+        while (data[i].userId === curID && i < data.length - 1) {
+            // console.log("Completed?: " + data[i].completed)
             if (data[i].completed === true) {
                 counter++;
             }
             i++;
         }
         listprint[curID] = counter;
-        curID = data[i].userId;
         counter = 0;
-    }})
+    }
+    console.log(listprint)
+})
 
 
 console.log('listprint: ' + listprint)
