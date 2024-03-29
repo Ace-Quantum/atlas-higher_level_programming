c34205b62ac19dd19f88a765df7993d9130321ed
root <root@DESKTOP-FADRK94>
2024-03-29 16:27:08 -0500

    Just try it

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -14,27 +14,14 @@ function CompareUserID(a, b) {
 request(url, function (error, response, body) {
     if (error) console.log(error);
     const data = JSON.parse(body);
-    // data.sort(CompareUserID)
 
     for (let i = 0; i <= data.length - 1; i++) {
         let curID = data[i].userId;
-        // console.log("user id: " + data[i].userId)
-        // console.log("curID: " + curID)
         while (data[i].userId === curID && i <= data.length - 1) {
-            // console.log("user id: " + data[i].userId)
-            // console.log("i: " + i)
             if (data[i].completed === true) {
                 counter++;
             }
             i++;
-            // if (data[i].userId != curID) {
-            //     // console.log('breaking');
-            //     i--;
-            //     break;
-            // }
-            // else {
-            //     // pass
-            // }
             if (i > data.length - 1) {
                 break
             }
@@ -44,7 +31,7 @@ request(url, function (error, response, body) {
             }
         }
         // i--;
-        listprint[curID] = counter;
+        listprint[curID] += counter;
         counter = 0;
         if (i > data.length - 1) {
             break
