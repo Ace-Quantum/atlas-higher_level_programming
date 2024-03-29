8518e7dfe2cde1d1d0293e6221d3a8a0ff29fff6
root <root@DESKTOP-FADRK94>
2024-03-29 11:21:56 -0500

    Might as well just try it

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,17 +1,25 @@
 #!/usr/bin/node
 
-const { JSDOM } = require('jsdom')
-
-const fs = require('fs')
-const file = fs.readFileSync('./tester.html', 'utf-8');
-
-const dom = new JSDOM(file);
-const doc = dom.window.document;
-
-const header = doc.querySelector("header");
-
 function MakeItRed() {
-    header.style.color = "#FF0000";
+    let header = document.getElementById("header")
+    header.style.color = "#FF0000"
 }
 
-header.addEventListener("load", MakeItRed)
+
+
+// Old code I no longer wish to see
+// const { JSDOM } = require('jsdom')
+
+// const fs = require('fs')
+// const file = fs.readFileSync('./tester.html', 'utf-8');
+
+// const dom = new JSDOM(file);
+// const doc = dom.window.document;
+
+// const header = doc.querySelector("header");
+
+// function MakeItRed() {
+//     header.style.color = "#FF0000";
+// }
+
+// dom.window.addEventListener("load", MakeItRed)
