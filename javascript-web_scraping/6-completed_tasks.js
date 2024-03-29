3e1f1c7bc5feb84aa51b55b35a27cf354341826a
root <root@DESKTOP-FADRK94>
2024-03-29 15:43:02 -0500

    I hate it here

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -18,7 +18,7 @@ request(url, function (error, response, body) {
 
     for (let i = 0; i < data.length - 1; i++) {
         let curID = data[i].userId;
-        // counter = 0;
+        counter = 0;
         while (data[i].userId === curID && i < data.length - 1) {
             if (data[i].completed === true) {
                 counter++;
