ac63344de2b4703b3092ece0b9c72ab7b52554ef
root <root@DESKTOP-FADRK94>
2024-03-29 15:00:12 -0500

    Let's see if the checker likes this one

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -7,9 +7,14 @@ const url = args[2];
 let counter = 0;
 let listprint = {};
 
+function CompareUserID(a, b) {
+    return a.userId - b.userId;
+}
+
 request(url, function (error, response, body) {
     if (error) console.log(error);
     const data = JSON.parse(body);
+    data.sort(CompareUserID)
 
     for (let i = 0; i < data.length - 1; i++) {
         let curID = data[i].userId;
@@ -24,4 +29,5 @@ request(url, function (error, response, body) {
     }
     console.log(listprint)
 })
+
 
