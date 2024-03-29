5456f50ce7935af79e91fb83a088e8624efb4cdd
root <root@DESKTOP-FADRK94>
2024-03-29 11:00:20 -0500

    I'd be afraid of spamming the checker but there's no checker to spam this time

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -5,7 +5,7 @@ const { JSDOM } = require('jsdom')
 const fs = require('fs')
 const file = fs.readFileSync('./tester.html', 'utf-8');
 
-const dom = new JSDOM(html);
+const dom = new JSDOM(file);
 const doc = dom.window.document;
 
 const header = doc.querySelector("header");
