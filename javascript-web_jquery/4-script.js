857b4c7a5cdd1ff9a1d188cba875307a1e3253e4
root <root@DESKTOP-FADRK94>
2024-03-30 19:24:18 -0500

    fail fast fail often

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
 $('#toggle_header').on('click', function() {
-  $('header').hasClass('red').toggleClass('green');
-//   $('header').hasClass('green').toggleClass('red');
+  $('header').toggleClass('green');
 })
