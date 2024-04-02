fae98d511b8ffb3b8e7d80c2df9c3718d967f101
root <root@DESKTOP-FADRK94>
2024-04-02 16:18:44 -0500

    I just wanna see what this will do

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,12 +1,10 @@
 #!/bin/usr/node
 
-$("#character").text(
-    $(function () {
-        $.ajax({
-            url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
-            success: function(data) {
-                return (data)
-            }
-        })
+$(function (){
+    $.ajax({
+        url: "https://swapi-api.hbtn.io/api/people/5/?format=json",
+        success: function(result){
+            $("#character").text(result)
+        }
     })
-)
+})
